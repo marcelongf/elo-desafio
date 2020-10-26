f6d97f39ef9df53f4d86fd621d13d1c40c212033
marcelongf <marcelongf@gmail.com>
2020-10-26 14:44:26 -0300

    react router configurado

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -16,18 +16,13 @@ export function Counter() {
   const dispatch = useDispatch();
   const [incrementAmount, setIncrementAmount] = useState('2');
 
-  const incrementFunc = () => {
-    toastr.success('funcionou');
-    dispatch(increment())
-  }
-
   return (
     <div>
       <div className={styles.row}>
         <button
           className={styles.button}
           aria-label="Increment value"
-          onClick={() => incrementFunc()}
+          onClick={() => dispatch(increment())}
         >
           +
         </button>
