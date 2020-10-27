470e32d98563620f3ea90b39edf9810b6a8db75d
marcelongf <marcelongf@gmail.com>
2020-10-27 05:06:15 -0300

    finalizações no registro

--- src/features/register/index.jsx
+++ src/features/register/index.jsx
@@ -6,23 +6,35 @@ import { useHistory } from 'react-router-dom';
 import Form from './form';
 import { registerNewUser, validateNewUser } from './actions';
 
+import logo from './logo.png';
+import './styles.css'
+
 const Register = () => {
   const dispatch = useDispatch();
   const history = useHistory();
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if(dispatch(validateNewUser())){
+    if (dispatch(validateNewUser())) {
       dispatch(registerNewUser(history));
     }
   }
 
+  const [buttonClass, setButtonClass] = React.useState("btn btn-dark")
+
   return (
-    <div className="container" >
-      <form onSubmit={e => handleSubmit(e)}>
-        <Form />
-        <button className="btn btn-success" >Registrar</button>
-      </form>
+    <div className="container">
+      <div className="register_container">
+        <img src={logo} className='register_logo_img' />
+        <form onSubmit={e => handleSubmit(e)}>
+          <Form />
+          <button
+            className={`${buttonClass}`}
+            onMouseOver={() => setButtonClass("btn btn-light")}
+            onMouseLeave={() => setButtonClass("btn btn-dark")}
+          >Registrar</button>
+        </form>
+      </div>
     </div>
   )
 }
